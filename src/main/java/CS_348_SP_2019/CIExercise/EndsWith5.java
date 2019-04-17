6b1c683739847adea52c93bdbb2425b97b8ef42c
Thanh Truong <ttruong9@worcester.edu>
2019-04-17 08:54:02 -0400

    ChangeWithMyName

--- src/main/java/CS_348_SP_2019/CIExercise/EndsWith5.java
+++ src/main/java/CS_348_SP_2019/CIExercise/EndsWith5.java
@@ -8,6 +8,7 @@ public class EndsWith5 {
 		names = "";
 		// add your name here in the form of a line that looks like:
 		// names+= "John Smith\n";
+		names+= "Thanh Truong\n";
 		
 	}
 
--- src/test/java/CS_348_SP_2019/CIExercise/EndsWith5Test.java
+++ src/test/java/CS_348_SP_2019/CIExercise/EndsWith5Test.java
@@ -11,7 +11,7 @@ public class EndsWith5Test {
 	public void testToString() {
 		EndsWith5 five = new EndsWith5();
 		// add your name to the expected string, after the all the other names in the form John Smith\n
-		assertEquals("EndsWith5 [names=]", five.toString());
+		assertEquals("EndsWith5 [names=Thanh Truong\n]", five.toString());
 
 	}
 
